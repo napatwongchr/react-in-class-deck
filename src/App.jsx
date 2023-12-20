d6ae316c1cf434c51e1310cebf3252d4f2e7b665
napatwongchr <napat.wongchr@gmail.com>
2023-12-20 10:29:43 +0700

    complete conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import GreetingMessage from "./components/GreetingMessage";
 
+// 1) โจทย์: เราต้องทำให้ component GreetingMessage แสดงผลแตกต่างกัน
+//     ตามรูปแบบที่กำหนด (Primary / Seconndary)
+// 2) ขั้นตอน
+//    - เปลี่ยนรูปแบบ style (css prop)
+//    - ใช้ prop ในการเปลี่ยนรูปแบบว่า primary / secondary
+//       ให้แสดงผลคนละแบบ
 function App() {
   return (
     <>
       <h1>Primary Greeting Message</h1>
-      <GreetingMessage text="Hello !" />
+      <GreetingMessage type="primary" text="Hello !" />
 
       <h1>Secondary Greeting Message</h1>
-      <GreetingMessage text="Hello !" />
+      <GreetingMessage type="secondary" text="Hello !" />
     </>
   );
 }
--- src/components/GreetingMessage.jsx
+++ src/components/GreetingMessage.jsx
@@ -1,4 +1,33 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
 function GreetingMessage(props) {
+  // props.type เป็น "primary" แสดงผลแบบ primary
+  // props.type เป็น "secondary" แสดงผลแบบ secondary
+  if (props.type === "primary") {
+    return (
+      <div
+        css={css`
+          border: 1px solid red;
+          text-decoration: underline red;
+          color: orange;
+        `}
+      >
+        <h1>{props.text}</h1>
+      </div>
+    );
+  } else if (props.type === "secondary") {
+    return (
+      <div
+        css={css`
+          color: green;
+        `}
+      >
+        <h1>{props.text}</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>{props.text}</h1>
